2b8ec1cc2aa7098c4e4b89f641a21ba5b1a41bf4
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-08-02 02:42:29 +0530

    completed basic functionalities for weather app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'weather-app-angular';
+export class AppComponent implements OnInit {
+  constructor(private weatherServise: WeatherService) {}
+
+  cityName: string = 'kolkata';
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherServise.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        // console.log(response);
+        this.weatherData = response;
+      },
+    });
+  }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { WeatherData } from '../models/weather.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +10,10 @@ import { Injectable } from '@angular/core';
 export class WeatherService {
 
   constructor(
-    private httpClient: HttpClient
+    private http: HttpClient
   ) { }
 
-  getWeatherData(cityName: string){
-    
+  getWeatherData(cityName: string): Observable<WeatherData>{
+    return this.http.get<WeatherData>(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${environment.WeatherApiKey}`);
   }
 }
