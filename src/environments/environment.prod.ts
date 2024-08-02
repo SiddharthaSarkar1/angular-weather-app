b0e46d9a855d2c70aa6e45b2f71fe7ebf1aa1f06
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-08-02 23:24:01 +0530

    minor update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     WeatherApiBaseUrl: "https://api.openweathermap.org/data/2.5/weather?q=kolkata&units=metric&APPID=",
-    WeatherApiKey: 'API_KEY_PLACEHOLDER'
+    WeatherApiKey: "",
   };
